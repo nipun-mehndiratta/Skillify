43e3e1f4323418bf531d36dfb4b2c3e5ff7fccd3
Nipun <123867733+nipun-mehndiratta@users.noreply.github.com>
2023-10-13 23:36:34 +0530

    UI bug fix

--- user-client/src/components/Course.tsx
+++ user-client/src/components/Course.tsx
@@ -45,7 +45,7 @@ if(!purchasedCourse){
          <Grid item xs={12} md={6} lg={6} style={{display:'flex',justifyContent:'center',flexDirection:'column',textAlign:'center'}}>
          <Typography style={{fontSize:'5vh'}} fontWeight={'bold'} color={'whitesmoke'}>{purchasedCourse.title}: {purchasedCourse.subtitle}</Typography>
           </Grid>
-          <div style={{width:'auto',maxWidth:'600px',maxHeight:'420px',backgroundColor:'inherit',paddingTop:'30px'}}>
+          <div style={{width:'auto',maxWidth:'600px',minHeight:'420px',backgroundColor:'inherit',paddingTop:'30px'}}>
              <Typography color={'#ffffff'} variant='h5' fontWeight={'bold'}>About This Class</Typography>
              <br/>
              <Typography color={'#c5cacf'}>{purchasedCourse.description}</Typography>
